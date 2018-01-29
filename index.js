3fc8b2f775dfe254f55e02c430cee800e5642c82
Tom Lawton <tom.lawton@cndu.it>
2018-01-29 18:39:38 +0000

    Load machines into pkg, instead of just their machine IDs.

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+// Transform for webpack
+var pkg = require('./package.json');
+var newMachines = [];
+
+for (var i in pkg.machinepack.machines) {
+
+  var id = pkg.machinepack.machines[i];
+
+  newMachines.push({
+    id: id,
+    def: require('./machines/' + id + '.js')
+  });
+}
+
+pkg.machinepack.machines = newMachines;
+
 module.exports = require('machine').pack({
-  pkg: require('./package.json'),
+  pkg: pkg,
   dir: __dirname
 });
